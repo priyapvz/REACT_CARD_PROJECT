7dd0206ec01418a84b700dc1c01205c6c068bbcf
KGCAS STUDENTS <casstudent@kgisledu.com>
2024-12-11 11:46:17 +0530

    CARD

---
+++ src/component/Card.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='maindiv'>
+        <div className='div1'>
+            <h1>VAISHU</h1>
+        </div>
+        <div className='div2'>
+            
+            <img src="image\girl.jpg" />
+        <p className='p1'>Hello!,this is vaishu.iam a content creator and also an artist.proficient in art and craft works.do follow me and subscribe to know more and develope yourself about the crafts.dm for the lovely hampers and gifts.</p>
+            <span style={{marginLeft:"60px"}}>105  100 122</span>
+            <p  style={{marginLeft:"30px"}}>Followers  Foloerb  hdsfguyg</p>
+            
+        </div>
+        <button className='b1' type='button' style={{marginLeft:"50px"}}>FOLLOW </button>
+        <button className='b2' type='button'  style={{marginLeft:"10px"}}>MESSAGE</button>
+    </div>
+  )
+}
+
+export default Card
